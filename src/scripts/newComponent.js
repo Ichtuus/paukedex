5ae773a031510a11194c3c9971f4629fd8c596cd
Ichtus <nicolasdiarra777@gmail.com>
2022-02-28 22:35:18 +0100

    feat(helperscript): Update script related to current project

--- src/scripts/newComponent.js
+++ src/scripts/newComponent.js
@@ -14,6 +14,7 @@ const camelNameComponent = utils.camelCase(nameComponent);
 const regexNameComponent = /{{cmpName}}/gm;
 const regexNameComponentPascalCase = /{{cmpNamePascalCase}}/gm;
 const regexNameComponentCamelCase = /{{cmpNameCamelCase}}/gm;
+const regexNameComponentKebabCase = /{{cmpNamekebabCase}}/gm;
 
 const componentsDir = `./src/components/${kebabNameComponent}`;
 
@@ -30,18 +31,20 @@ if (nameComponent) {
     `${__dirname}/templates/componentClass.template`,
     "utf8"
   );
-
   classTemplate = classTemplate
-    .replace(regexNameComponent, kebabNameComponent)
+    .replace(regexNameComponentCamelCase, camelNameComponent)
+    .replace(regexNameComponentKebabCase, kebabNameComponent)
     .replace(regexNameComponentPascalCase, pascalNameComponent);
 
-  // read the html template
-  let htmlTemplate = fs.readFileSync(
-    `${__dirname}/templates/componentHTML.template`,
+  // read the css template
+  let cssTemplate = fs.readFileSync(
+    `${__dirname}/templates/componentCss.template`,
     "utf8"
   );
-  htmlTemplate = htmlTemplate.replace(regexNameComponent, kebabNameComponent);
-
+  cssTemplate = cssTemplate.replace(
+    regexNameComponentCamelCase,
+    camelNameComponent
+  );
   ///////////
   // WRITE //
   ///////////
@@ -56,68 +59,15 @@ if (nameComponent) {
   );
   console.log(`Created ${componentsDir}/${kebabNameComponent}.ts`);
 
-  // html file
+  // css file
   fs.writeFileSync(
-    `${componentsDir}/${kebabNameComponent}.html`,
-    htmlTemplate,
-    function (err) {
-      if (err) return console.log(err);
-    }
-  );
-  console.log(`Created ${componentsDir}/${kebabNameComponent}.html`);
-
-  // scss file
-  fs.writeFileSync(
-    `${componentsDir}/_${kebabNameComponent}.module.scss`,
-    "",
+    `${componentsDir}/_${kebabNameComponent}.css.ts`,
+    cssTemplate,
     function (err) {
       if (err) return console.log(err);
     }
   );
   console.log(`Created ${componentsDir}/${kebabNameComponent}.module.scss`);
-
-  // update index.ts
-  let index = fs.readFileSync("./src/index.ts", "utf8");
-
-  // find last wrap
-  let lastWrapPositionEnd = index.indexOf("customElements");
-  lastWrapPositionEnd =
-    lastWrapPositionEnd === -1 ? index.length : lastWrapPositionEnd;
-  const customElementsArr = index.match(
-    /customElements.define['a-zA-Z0-9-, ()=>.\/]*;\n?/g
-  );
-
-  if (customElementsArr) {
-    customElementsArr.forEach((item, offset) => {
-      lastWrapPositionEnd += item.length;
-    });
-  }
-
-  // insert wrap function for dynamic import
-  const wrapDynamicImport = `customElements.define(\'${kebabNameComponent}\', wrap(()=>import(\'./components/${kebabNameComponent}/${kebabNameComponent}\'), \'${pascalNameComponent}\', observedAttributes));\n`;
-  index =
-    index.slice(0, lastWrapPositionEnd) +
-    wrapDynamicImport +
-    index.slice(lastWrapPositionEnd);
-
-  // insert wrap function for dynamic import
-  fs.writeFileSync("./src/index.ts", index, function (err) {
-    if (err) return console.log(err);
-  });
-  console.log(`Imported ${kebabNameComponent} to index.ts`);
-
-  // GIT ADD
-  // exec(`git add ${componentsDir}`, (error, stdout, stderr) => {
-  //   if (error) {
-  //     console.log(`error: ${error.message}`);
-  //     return;
-  //   }
-  //   if (stderr) {
-  //     console.log(`stderr: ${stderr}`);
-  //     return;
-  //   }
-  //   console.log(`Added ${kebabNameComponent} components to git`);
-  // });
 } else {
   console.error("No component's name");
 }
